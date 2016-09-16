88914ebf77c4ce6ad8da1d77f2d39f19447a3d3c
almondtools <mandelst@gmail.com>
2016-09-16 07:29:07 +0200

    dependency update and some more benchmarks

---
+++ src/main/java/com/almondtools/stringbench/singlepattern/stringsearchalgorithms/SCHorspoolRelaxedBenchmark.java
@@ -0,0 +1,33 @@
+package com.almondtools.stringbench.singlepattern.stringsearchalgorithms;
+
+import static com.almondtools.stringbenchanalyzer.Family.SUFFIX;
+
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import com.almondtools.stringbenchanalyzer.Family;
+
+import net.amygdalum.stringsearchalgorithms.search.Horspool;
+import net.amygdalum.stringsearchalgorithms.search.StringSearchAlgorithm;
+
+@State(Scope.Thread)
+public class SCHorspoolRelaxedBenchmark extends StringSearchAlgorithmsBenchmark {
+
+	private static final String ID = "Strings and Chars Horspool (non-strict shifts)";
+
+	@Override
+	public String getId() {
+		return ID;
+	}
+	
+	@Override
+	public Family getFamily() {
+		return SUFFIX;
+	}
+
+	@Override
+	public StringSearchAlgorithm create(String pattern) {
+		return new Horspool(pattern, true);
+	}
+
+}
