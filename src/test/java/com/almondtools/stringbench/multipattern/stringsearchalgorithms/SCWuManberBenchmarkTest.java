944652e078671cc5438ab402aaba03a24f5a4ed9
almondtools <mandelst@gmail.com>
2016-10-30 12:07:41 +0100

    version update

--- src/test/java/com/almondtools/stringbench/multipattern/stringsearchalgorithms/SCWuManberBenchmarkTest.java
+++ src/test/java/com/almondtools/stringbench/multipattern/stringsearchalgorithms/SCWuManberBenchmarkTest.java
@@ -1,5 +1,7 @@
 package com.almondtools.stringbench.multipattern.stringsearchalgorithms;
 
+import org.junit.Test;
+
 import com.almondtools.stringbench.multipattern.MultiPatternMatcherBenchmark;
 import com.almondtools.stringbench.multipattern.MultiPatternMatcherBenchmarkTest;
 
@@ -8,4 +10,10 @@ public class SCWuManberBenchmarkTest extends MultiPatternMatcherBenchmarkTest {
 	protected MultiPatternMatcherBenchmark getBenchmark() {
 		return new SCWuManberBenchmark();
 	}
+
+	@Test
+	public void testBenchmarkFindInFile_1024_1024_2() throws Exception {
+		findInFileSample(1024, 1024, 2);
+	}
+
 }
