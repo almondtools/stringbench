7f84aabcf5135c13923e89090ba2bd0e66d52a06
almondtools <mandelst@gmail.com>
2018-05-03 21:08:03 +0200

    formatting

--- src/main/java/com/almondtools/stringbench/analyzer/SampleGenerator.java
+++ src/main/java/com/almondtools/stringbench/analyzer/SampleGenerator.java
@@ -14,14 +14,15 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class SampleGenerator {
+
 	public static void main(String[] args) throws IOException {
 		String bytes = new String(Files.readAllBytes(Paths.get("src/main/resources/sample-kjb.sample")), StandardCharsets.UTF_8);
 		List<String> tokens = tokens(Files.readAllLines(Paths.get("src/main/resources/sample-kjb.sample")));
-		
+
 		try (BufferedWriter w = Files.newBufferedWriter(Paths.get("src/main/resources/pattern-kjb.result"))) {
 			Random r = new Random();
 			Set<String> done = new HashSet<>();
-			for (int i =0; i < 20000; i++) {
+			for (int i = 0; i < 20000; i++) {
 				String pattern = generatePattern(r, tokens, done);
 
 				w.write(pattern + ":" + findAll(bytes, pattern));
@@ -41,7 +42,6 @@ public class SampleGenerator {
 				tokens.add(tokenizer.nextToken());
 			}
 		}
-		System.out.println(tokens.size());
 		return new ArrayList<>(tokens);
 	}
 
@@ -59,7 +59,7 @@ public class SampleGenerator {
 	public static String computePattern(String bytes, Random r) {
 		int rnd = r.nextInt(bytes.length());
 		int len = 5 + r.nextInt(5);
-		return bytes.substring(rnd, rnd+len);
+		return bytes.substring(rnd, rnd + len);
 	}
 
 	private static int findAll(String bytes, String pattern) {
