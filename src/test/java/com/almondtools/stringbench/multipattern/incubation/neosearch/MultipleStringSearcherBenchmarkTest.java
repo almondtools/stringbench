150cf05d2102ced54a179fde00665c4bde23892f
Stefan Mandel <mandelst@gmail.com>
2024-04-17 11:38:09 +0200

    only keep one example failing test

--- src/test/java/com/almondtools/stringbench/multipattern/incubation/neosearch/MultipleStringSearcherBenchmarkTest.java
+++ src/test/java/com/almondtools/stringbench/multipattern/incubation/neosearch/MultipleStringSearcherBenchmarkTest.java
@@ -1,14 +1,25 @@
 package com.almondtools.stringbench.multipattern.incubation.neosearch;
 
-import com.almondtools.stringbench.multipattern.MultiPatternMatcherBenchmark;
-import com.almondtools.stringbench.multipattern.MultiPatternMatcherBenchmarkTest;
+import org.junit.Rule;
+import org.junit.Test;
 
-public class MultipleStringSearcherBenchmarkTest extends MultiPatternMatcherBenchmarkTest {
+import com.almondtools.stringbench.CompareResultNotAccepted;
+import com.almondtools.stringbench.StringBenchIncubation;
+import com.almondtools.stringbench.multipattern.MultiPatternSample;
+import com.almondtools.stringbench.multipattern.MultiPatternTest;
 
-    @Override
-    protected MultiPatternMatcherBenchmark getBenchmark() {
-        return new MultipleStringSearcherBenchmark();
+public class MultipleStringSearcherBenchmarkTest extends MultiPatternTest {
+
+    @Rule
+    public StringBenchIncubation incubation = new StringBenchIncubation(new MultipleStringSearcherBenchmark());
+    
+    @Rule
+    public CompareResultNotAccepted compare = CompareResultNotAccepted.compare();
+    
+    @Test
+    public void test222() throws Exception {
+        MultiPatternSample sample = createSample(2, 2, 2);
+        incubation.benchmarkFindInString(sample);
     }
-
 
 }
